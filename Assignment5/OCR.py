13665cc8b57135856dd2874e03217812b44e99d5
Morten <morteano@stud.ntnu.no>
2016-04-17 17:46:55 +0200

    add canny edge detector, and remove gamma value in SVM

--- Assignment5/OCR.py
+++ Assignment5/OCR.py
@@ -7,6 +7,11 @@ from sklearn import datasets, svm, metrics
 from scipy import ndimage
 import PIL
 import time
+from skimage.restoration import denoise_tv_chambolle, denoise_bilateral
+from skimage.filters import roberts, sobel, scharr, prewitt
+from skimage import feature
+from skimage import img_as_bool
+from skimage.morphology import black_tophat, skeletonize, convex_hull_image
 
 path = './chars74k-lite/'
 
@@ -27,6 +32,11 @@ def loadImages():
             for filename in os.listdir(path+directory+'/'):
                 img = sp.misc.imread(path+directory+'/'+filename)
                 img = img.astype(np.float)/255
+                # printImage(img)
+                img = denoise_bilateral(img, sigma_range=0.1, sigma_spatial=15)
+                # printImage(img)
+                img = feature.canny(img)
+                # printImage(img)
                 imagePairs.images.append(img)
                 imagePairs.letters.append(directory)
     return imagePairs
@@ -62,13 +72,13 @@ def reshape(images):
 def main():
     dataSet = loadImages()
     dataSet, testSet = splitDataset(0.1, dataSet)
-    images_and_labels = list(zip(dataSet.images, dataSet.letters))
+    # images_and_labels = list(zip(dataSet.images, dataSet.letters))
     # for index, (image, label) in enumerate(images_and_labels[:4]):
     #     plt.subplot(2, 4, index + 1)
     #     plt.axis('off')
     #     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     #     plt.title('Training: ' + label)
-    classifier = svm.SVC(gamma=0.001)
+    classifier = svm.SVC()
     classifier.fit(reshape(dataSet.images), dataSet.letters)
     predicted = classifier.predict(reshape(testSet.images))
     images_and_predictions = list(zip(testSet.images, predicted))
@@ -77,22 +87,23 @@ def main():
     for index, (image, prediction) in enumerate(images_and_predictions):
         if prediction == testSet.letters[index]:
             correct += 1
+            print("Correct:", prediction, testSet.letters[index])
         else:
             false += 1
-            print(prediction, testSet.letters[index])
+            print("False:", prediction, testSet.letters[index])
     print(correct/(correct+false))
 
-        # plt.subplot(2, 4, index + 5)
-        # plt.axis('off')
-        # plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-        # plt.title('Prediction: ' + prediction)
-        # print(testSet.letters[index])
+    # plt.subplot(2, 4, index + 5)
+    # plt.axis('off')
+    # plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    # plt.title('Prediction: ' + prediction)
+    # print(testSet.letters[index])
     # plt.show()
 
 main()
 
 
 
-
+# /255, denoise(0.05,15), sobel -> 37.8 %
 
 
